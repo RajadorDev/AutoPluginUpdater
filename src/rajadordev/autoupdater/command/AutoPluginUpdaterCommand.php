dd340de89c3454cdafabfebede0d7edab5a73e1d
RajadorDev <rajadordeveloper@gmail.com>
2026-02-25 19:27:22 -0300

    Subcommands registered

--- src/rajadordev/autoupdater/command/AutoPluginUpdaterCommand.php
+++ src/rajadordev/autoupdater/command/AutoPluginUpdaterCommand.php
@@ -20,6 +20,8 @@ declare (strict_types=1);
 namespace rajadordev\autoupdater\command;
 
 use pocketmine\command\CommandSender;
+use rajadordev\autoupdater\command\subcommand\SetGitTokenSubCommand;
+use rajadordev\autoupdater\command\subcommand\VersionSubCommand;
 use rajadordev\autoupdater\Loader;
 use SmartCommand\command\CommandArguments;
 use SmartCommand\command\SmartCommand;
@@ -35,6 +37,10 @@ class AutoPluginUpdaterCommand extends SmartCommand
     protected function prepare()
     {
         $this->setPrefix(Loader::PREFIX);
+        $this->registerSubCommands([
+            new VersionSubCommand($this, 'version', 'Shows APU version', ['ver']),
+            new SetGitTokenSubCommand($this, 'setghtoken', 'Set the GitHub token')
+        ]);
     }
 
     protected function onRun(CommandSender $sender, string $label, CommandArguments $args)
